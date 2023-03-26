da832b351cbea0def7afc53197b6a9296618e5eb
ραяιкѕнιт <87859520+Parixshit@users.noreply.github.com>
2023-03-27 00:34:54 +0530

    Add commit message in file
    Signed-off-by: ραяιкѕнιт <87859520+Parixshit@users.noreply.github.com>

--- auto_commit.sh
+++ auto_commit.sh
@@ -12,15 +12,16 @@ touch LAST_UPDATED
 # Loop for the random number of commits
 for (( i=1; i<=$num_commits; i++ ))
 do
-    # Update the LAST_UPDATED file with the current date and time and the total number of commits
+    # Update the LAST_UPDATED file with the current date and time, the total number of commits, and the commit message
     current_time=$(date "+%Y-%m-%d %H:%M:%S")
-    echo "Current date and time: $current_time.\nTotal number of commits: $tot_commits" > LAST_UPDATED
+    commit_message="Commit $i of $num_commits 🚀 $current_time"
+    echo -e "Current date and time: $current_time.\nTotal number of commits: $tot_commits.\nCommit message: $commit_message\n" > LAST_UPDATED
 
     # Add LAST_UPDATED file to git
     git add LAST_UPDATED
 
-    # Commit the changes with a message containing the current commit number, total number of commits, a rocket emoji, and the current time
-    git commit -s -m "Commit $i of $num_commits 🚀 $current_time"
+    # Commit the changes with a message containing the commit message and the current time
+    git commit -s -m "$commit_message"
 
     # Sleep for 1 second to add some randomness
     sleep 1
