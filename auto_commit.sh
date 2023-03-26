183295771f446dd055eb6265014cc8b1981b0856
ραяιкѕнιт <87859520+Parixshit@users.noreply.github.com>
2023-03-26 20:40:48 +0530

    Current iteration
    Signed-off-by: ραяιкѕнιт <87859520+Parixshit@users.noreply.github.com>

--- auto_commit.sh
+++ auto_commit.sh
@@ -16,8 +16,8 @@ do
     # Add LAST_UPDATED file to git
     git add LAST_UPDATED
 
-    # Commit the changes with a message
-    git commit -s -m "$num_commits - $current_time"
+    # Commit the changes with a message containing the current iteration and the current time
+    git commit -s -m "$i - $current_time"
 
     # Sleep for 1 second to add some randomness
     sleep 1
